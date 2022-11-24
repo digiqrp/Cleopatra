565deed3c79186005ed3eb9a230c7500c2e25ff5
Mark Gregory <mark.gregory@gmx.com>
2022-11-24 16:45:13 +0000

    added eCommerce added additional design pages

--- resources/views/ecommerce.blade.php
+++ resources/views/ecommerce.blade.php
@@ -3,7 +3,11 @@
         <x-side-bar></x-side-bar>
         <!-- Start Content -->
         <div class="bg-gray-100 flex-1 p-6 md:mt-16">
-        <x-congrats></x-congrats>
+            <x-congrats></x-congrats>
+            <x-summary></x-summary>
+            <x-status></x-status>
+            <x-best-seller></x-best-seller>
+            <x-recent-orders></x-recent-orders>
         </div>
         <!-- end content -->
     </div>
